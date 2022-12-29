1922606914c70d2be9a984801ab2e10ffe6dde74
prasad440 <prasadkovvuri440@gmail.com>
2022-12-29 14:38:46 +0530

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import smtplib
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -136,3 +138,17 @@ STATICFILES_DIRS = [
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'kovvuridurgap@gmail.com'
+EMAIL_HOST_PASSWORD = 'qucncsmuaezqzbrn'
+EMAIL_USE_TLS = True
