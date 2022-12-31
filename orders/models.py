a67d506ff924b1c7163f78f20475a42a9fc05839
prasad440 <prasadkovvuri440@gmail.com>
2022-12-31 16:35:25 +0530

    product order and payments

--- orders/models.py
+++ orders/models.py
@@ -54,14 +54,14 @@ class Order(models.Model):
     def __str__(self):
         return self.first_name
 
+
+
 class OrderProduct(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     payment = models.ForeignKey(Payment, on_delete=models.SET_NULL, blank=True, null=True)
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     variations = models.ManyToManyField(Variation, blank=True)
-    color = models.CharField(max_length=50)
-    size = models.CharField(max_length=50)
     quantity = models.IntegerField()
     product_price = models.FloatField()
     ordered = models.BooleanField(default=False)
