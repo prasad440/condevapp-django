1ddd7888cec0d32a8fcbdc2b8ba9bd3149c70172
prasad440 <prasadkovvuri440@gmail.com>
2023-01-01 21:14:47 +0530

    review and rating system

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Product
+from .models import Product, ReviewRating
 from category.models import Category
 from carts.models import CartItem
 from django.db.models import Q
@@ -42,16 +42,26 @@ def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
         in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
-
     except Exception as e:
         raise e
+
+    if request.user.is_authenticated:
+        try:
+            orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+        except OrderProduct.DoesNotExist:
+            orderproduct = None
+    else:
+        orderproduct = None
+
+    # Get the reviews
+    reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
     context = {
         'single_product': single_product,
         'in_cart'       : in_cart,
-
+        'orderproduct': orderproduct,
+        'reviews': reviews,
     }
-
     return render(request, 'store/product_detail.html', context)
 
 def search(request):
